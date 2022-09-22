13d9025fe242b6346e586ef416bffda66896618d
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2022-09-23 00:23:26 +0300

    hw-07-04

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,25 +21,38 @@ function createListGallery(galleryItems) {
     })
     .join("");
 }
-// додаємо слухача подій на контейнер
-galleryRef.addEventListener("click", onGalleryRefClick);
-// обмеження стандартних дій браузера
-function onGalleryRefClick(evt) {
+// // додаємо слухача подій на контейнер
+galleryRef.addEventListener("click",  handleClick);
+function  handleClick(evt) {
+// // обмеження стандартних дій браузера
   evt.preventDefault();
-  //   перевірка кліку _ якщо не "IMG", виходимо з функції
+//   //   перевірка кліку _ якщо не "IMG", виходимо з функції
   if (evt.target.nodeName !== "IMG") {
     return;
   }
-  // додаємо розмітку для модалки
+  // Заміна значення атрибута src елемента <img> в модальному вікні
   const newImgRef = evt.target.dataset.source;
-  const instance = basicLightbox.create(`<div class="modal">
-<img src ="${newImgRef}" width="800" height="600"> </div>
-`);
-  instance.show();
-  // додаємо слухача подій длязакриттямодалки через клавішу "ESC"
-  galleryRef.addEventListener("keydown", (evt) => {
-    if (evt.code === "Escape") {
+  const instance = basicLightbox.create(
+    `<div class="modal">
+    <img src ="${newImgRef}" width="800" height="600"> </div>
+    `,
+    {
+      // закриття модального вікна після натискання клавіші Escape, Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
+      onShow: (instance) => {
+        document.addEventListener("keydown", onEscape);
+      },
+
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onEscape);
+      },
+    }
+  );
+  // Callback функція для зикривання модалки при кліку на esc 
+  function onEscape(event) {
+    if (event.key === "Escape") {
       instance.close();
     }
-  });
-};
+  }
+  instance.show();
+}
+
